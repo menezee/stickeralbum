f45c42b5b0eaeff446698eb0bc081f175d453a5d
Erich Menezes <emenezes@thoughtworks.com>
2018-04-05 01:41:57 -0300

    add react scaffold from create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <p>
+            My first react app
+        </p>
+    );
+  }
+}
+
+export default App;
